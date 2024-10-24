1b497bfc0eb4117c9f4190c7852ee9f11c8f5b6c
Caleb Roskelley <caleb.roskelley@students.snow.edu>
2024-10-23 21:35:10 -0600

    started code, prints maze

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] mapRows = File.ReadAllLines("map.txt");
+        programIntro();
+        Console.ReadKey();
+        Console.Clear();
+        foreach(string row in mapRows)
+        {
+            Console.WriteLine(row);
+        }
+    }
+    static void programIntro()
+    {
+        Console.WriteLine("This program will present a maze for you to move through using directional arrows, your goal is to reach the *");
+        Console.WriteLine("Goodluck! Press any button to continue");
+    }
+}
